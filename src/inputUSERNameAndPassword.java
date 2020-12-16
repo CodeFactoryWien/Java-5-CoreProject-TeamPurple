eb6aea60211afbb4bd9dce43f92d8eb1ae4f07ca
Malbadr1 <Albadri@gmx.net>
2020-12-16 12:23:11 +0100

    username and password+

--- src/inputUSERNameAndPassword.java
+++ src/inputUSERNameAndPassword.java
@@ -1,77 +1,56 @@
-
-    import java.sql.*;
+import java.sql.*;
 import java.util.Scanner;
-
-    public class  inputUSERNameAndPassword{
-        static Scanner scanner = new Scanner(System.in);
-        static final String JDBC_Driver = "com.mysql.cj.jdbc.Driver";
-        static final String url = "jdbc:mysql://localhost:3306/hotel";
-        private static final String USERNAME = "root";
-        private static final String PASSWORD = "";
-
-        Connection connection = null;
-
-
-
-        public void showInputUSERNameAndPassword() {
-
+public class  inputUSERNameAndPassword{
+    static Scanner scanner = new Scanner(System.in);
+    static final String JDBC_Driver = "com.mysql.cj.jdbc.Driver";
+    static final String url = "jdbc:mysql://localhost:3306/hotel";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    Connection connection = null;
+    public void showInputUSERNameAndPassword() {
+        try {
+            Class.forName(JDBC_Driver);
+            connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
             try {
-                Class.forName(JDBC_Driver);
-
-                connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
-
-
-
-
-                try {
-                    System.out.println("Enter username");
-                    Scanner scc = new Scanner(System.in);
-
-                    String inputUser= scc.next();
-                    System.out.println("Enter password");
-
-                    String inputPassword= scc.next();
-                    PreparedStatement passWordDatabase = connection.prepareStatement("SELECT staff.password FROM staff");
-                    ResultSet rs = passWordDatabase.executeQuery();
-
-                    PreparedStatement usernameDatabase = connection.prepareStatement("SELECT staff.username FROM staff");
-                    ResultSet rs1 = usernameDatabase.executeQuery();
-
-                        while (rs.next()&&rs1.next()) {
-                            String usernameDatabase1= rs1.getString("username");
-                            String passWordDatabase1= rs.getString("password");
-
-                            if (inputUser.equals(usernameDatabase1) && inputPassword.equals(passWordDatabase1)) {
-                                System.out.println("welcome");
-                                break;
-                            }
-                        }
+                System.out.println("Enter username");
+                Scanner scc = new Scanner(System.in);
+                String inputUser= scc.next();
+                System.out.println("Enter password");
+                String inputPassword= scc.next();
+                PreparedStatement passWordDatabase = connection.prepareStatement("SELECT staff.password FROM staff");
+                ResultSet rs = passWordDatabase.executeQuery();
+                PreparedStatement usernameDatabase = connection.prepareStatement("SELECT staff.username FROM staff");
+                ResultSet rs1 = usernameDatabase.executeQuery();
+                while (rs.next()&&rs1.next()) {
                     String usernameDatabase1= rs1.getString("username");
                     String passWordDatabase1= rs.getString("password");
-                    if (!inputUser.equals(usernameDatabase1) && !inputPassword.equals(passWordDatabase1)){
-                        System.out.println("\"Enter Valid username and password");
-                        connection.close();
+                    if (inputUser.equals(usernameDatabase1) && inputPassword.equals(passWordDatabase1)) {
+                       Menu me= new Menu(Main.dataAccess);
+
+                        break;
                     }
-                }catch (Exception e) {
-                    System.out.println("Enter Valid username and password");
+                }
+                String usernameDatabase1= rs1.getString("username");
+                String passWordDatabase1= rs.getString("password");
+                if (!inputUser.equals(usernameDatabase1) && !inputPassword.equals(passWordDatabase1)){
+                    System.out.println("\"Enter Valid username and password");
                     connection.close();
                 }
+            }catch (Exception e) {
+                System.out.println("Enter Valid username and password");
                 connection.close();
-
-
-            } catch (SQLException sql) {
-                sql.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-
             }
+            connection.close();
+        } catch (SQLException sql) {
+            sql.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        public static void main(String[] args) {
-            inputUSERNameAndPassword in= new inputUSERNameAndPassword();
-            in.showInputUSERNameAndPassword();
-        }
-
     }
+    public static void main(String[] args) {
+        inputUSERNameAndPassword in= new inputUSERNameAndPassword();
+        in.showInputUSERNameAndPassword();
+    }
+}
 
 
