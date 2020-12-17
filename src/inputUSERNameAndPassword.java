791d4898ba26fe442ceba09cd35e1ef448a2e5bc
Michael Schano <michael.schano@gmx.at>
2020-12-17 12:40:35 +0100

    changed back to normal the password2

--- src/inputUSERNameAndPassword.java
+++ src/inputUSERNameAndPassword.java
@@ -17,18 +17,15 @@ public class  inputUSERNameAndPassword{
                 String inputUser= scc.next();
                 System.out.println("Enter password");
                 String inputPassword= scc.next();
-                PreparedStatement passWordDatabase = connection.prepareStatement("SELECT stuff.id, staff.password FROM staff");
+                PreparedStatement passWordDatabase = connection.prepareStatement("SELECT staff.password FROM staff");
                 ResultSet rs = passWordDatabase.executeQuery();
                 PreparedStatement usernameDatabase = connection.prepareStatement("SELECT staff.username FROM staff");
                 ResultSet rs1 = usernameDatabase.executeQuery();
                 while (rs.next()&&rs1.next()) {
                     String usernameDatabase1= rs1.getString("username");
                     String passWordDatabase1= rs.getString("password");
-                    int userId=rs.getInt("id");
                     if (inputUser.equals(usernameDatabase1) && inputPassword.equals(passWordDatabase1)) {
-                        AddNewBooking.userName=usernameDatabase1;
-                        AddNewBooking.userId=userId;
-                        Menu me= new Menu(Main.dataAccess);
+                       Menu me= new Menu(Main.dataAccess);
 
                         break;
                     }
